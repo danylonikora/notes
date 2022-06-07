e2503db6849ef365d4d5b62dd52e114b849ea03f
Danylo Nikora <d.nikora2019@gmail.com>
2022-06-07 17:12:18 +0300

    Add Overlay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import Note, { NoteT } from "./components/Note";
 import SideBar from "./components/SideBar";
 import Draft from "./components/Draft";
+import Overlay, { OverlayProps } from "./components/Overlay";
 
 export type FiltersT = {
   sort: "ascending" | "descending";
   includes: string;
   tag: string;
 };
+
+type OverlaysContextProps = {
+  overlaysProps: OverlayProps;
+  showOverlays: boolean;
+  mountOverlays: (
+    handleClick: OverlayProps["handleClick"],
+    overlayedElementRef: OverlayProps["overlayedElementRef"]
+  ) => void;
+  unmountOverlays: () => void;
+};
+
+export const OverlaysContext = createContext<OverlaysContextProps>({
+  overlaysProps: { overlayedElementRef: undefined, handleClick: undefined },
+  showOverlays: false,
+  mountOverlays: () => undefined,
+  unmountOverlays: () => undefined,
+});
 
 function App() {
   const [notes, setNotes] = useState<NoteT[]>(
@@ -20,6 +38,11 @@ function App() {
     sort: "ascending",
     includes: "",
     tag: "",
+  });
+  const [showOverlays, setShowOverlays] = useState(false);
+  const [overlaysProps, setOverlaysProps] = useState<OverlayProps>({
+    overlayedElementRef: undefined,
+    handleClick: undefined,
   });
 
   function applyFilters(notes: NoteT[]) {
@@ -74,73 +97,93 @@ function App() {
   }, [tags]);
 
   return (
-    <div className="page-container">
-      <SideBar
-        tags={[...tags]}
-        filters={filters}
-        setTag={(tag) => setFilters((prevFilters) => ({ ...prevFilters, tag }))}
-        setIncludes={(string) =>
-          setFilters((prevFilters) => ({ ...prevFilters, includes: string }))
-        }
-        setSort={(order) =>
-          setFilters((prevFilters) => ({ ...prevFilters, sort: order }))
-        }
-        addTag={(tag) => setTags((prevTags) => new Set([...prevTags, tag]))}
-        deleteTag={(tag) =>
-          setTags((prevTags) => {
-            const newTags = new Set([...prevTags].filter((t) => t !== tag));
-            setNotes((prevNotes) =>
-              prevNotes.map((note) => {
-                if (note.tags.includes(tag)) {
-                  note.tags = note.tags.filter((t) => t !== tag);
-                }
-                return note;
-              })
-            );
-            return newTags;
-          })
-        }
-        renameTag={(tag, newName) =>
-          setTags((prevTags) => {
-            const newTags = new Set(
-              [...prevTags].map((t) => (t === tag ? newName : t))
-            );
-            setNotes((prevNotes) =>
-              prevNotes.map((note) => {
-                if (note.tags.includes(tag)) {
-                  note.tags[note.tags.indexOf(tag)] = newName;
-                }
-                return note;
-              })
-            );
-            return newTags;
-          })
-        }
-      />
-      <div className="notes">
-        {applyFilters(notes).map((note) => (
-          <Note
-            key={note.id}
-            data={note}
-            setText={changeNoteDecorator(note.id, "text")}
-            deleteNote={(id) => {
-              // getNoteById will throw error if it won't find note with given id
-              getNoteById(id);
-              setNotes((prevNotes) => [
-                ...prevNotes.filter((note) => note.id !== id),
-              ]);
-            }}
-            setTags={changeNoteDecorator(note.id, "tags")}
-            allTags={[...tags]}
-          />
-        ))}
-        {notes.length === 0 && "You don't have notes"}
+    <OverlaysContext.Provider
+      value={{
+        overlaysProps,
+        showOverlays,
+        mountOverlays(handleClick, overlayedElementRef) {
+          setOverlaysProps({ handleClick, overlayedElementRef });
+          setShowOverlays(true);
+        },
+        unmountOverlays() {
+          setShowOverlays(false);
+        },
+      }}
+    >
+      <div className="page-container">
+        <SideBar
+          tags={[...tags]}
+          filters={filters}
+          setTag={(tag) =>
+            setFilters((prevFilters) => ({ ...prevFilters, tag }))
+          }
+          setIncludes={(string) =>
+            setFilters((prevFilters) => ({
+              ...prevFilters,
+              includes: string,
+            }))
+          }
+          setSort={(order) =>
+            setFilters((prevFilters) => ({ ...prevFilters, sort: order }))
+          }
+          addTag={(tag) => setTags((prevTags) => new Set([...prevTags, tag]))}
+          deleteTag={(tag) =>
+            setTags((prevTags) => {
+              const newTags = new Set([...prevTags].filter((t) => t !== tag));
+              setNotes((prevNotes) =>
+                prevNotes.map((note) => {
+                  if (note.tags.includes(tag)) {
+                    note.tags = note.tags.filter((t) => t !== tag);
+                  }
+                  return note;
+                })
+              );
+              return newTags;
+            })
+          }
+          renameTag={(tag, newName) =>
+            setTags((prevTags) => {
+              const newTags = new Set(
+                [...prevTags].map((t) => (t === tag ? newName : t))
+              );
+              setNotes((prevNotes) =>
+                prevNotes.map((note) => {
+                  if (note.tags.includes(tag)) {
+                    note.tags[note.tags.indexOf(tag)] = newName;
+                  }
+                  return note;
+                })
+              );
+              return newTags;
+            })
+          }
+        />
+        <div className="notes">
+          <Overlay />
+          {applyFilters(notes).map((note) => (
+            <Note
+              key={note.id}
+              data={note}
+              setText={changeNoteDecorator(note.id, "text")}
+              deleteNote={(id) => {
+                // getNoteById will throw error if it won't find note with given id
+                getNoteById(id);
+                setNotes((prevNotes) => [
+                  ...prevNotes.filter((note) => note.id !== id),
+                ]);
+              }}
+              setTags={changeNoteDecorator(note.id, "tags")}
+              allTags={[...tags]}
+            />
+          ))}
+          {notes.length === 0 && "You don't have notes"}
+        </div>
+        <Draft
+          addNote={(note) => setNotes((prevNotes) => [...prevNotes, note])}
+          allTags={[...tags]}
+        />
       </div>
-      <Draft
-        addNote={(note) => setNotes((prevNotes) => [...prevNotes, note])}
-        allTags={[...tags]}
-      />
-    </div>
+    </OverlaysContext.Provider>
   );
 }
 
---
+++ src/components/Overlay.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useContext } from "react";
+import { OverlaysContext } from "../App";
+
+export type OverlayProps = {
+  overlayedElementRef: React.MutableRefObject<HTMLElement> | undefined;
+  handleClick: (() => void) | undefined;
+};
+
+// In order to overlay entire page Overlay components
+// have to be present in every stacking context; docs -
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context
+
+function Overlay() {
+  const divRef = useRef();
+  const OverlayContext = useContext(OverlaysContext);
+  const { handleClick, overlayedElementRef } = OverlayContext.overlaysProps;
+
+  useEffect(() => {
+    if (!divRef.current || !overlayedElementRef?.current) return;
+
+    // <Overlay's /> z-index is set to 998 in scss
+    overlayedElementRef.current.style.zIndex = "999";
+
+    return () => {
+      if (!overlayedElementRef?.current) return;
+      delete overlayedElementRef.current.style.zIndex;
+    };
+  });
+
+  return (
+    OverlayContext.showOverlays && (
+      <div
+        className="overlay"
+        ref={divRef}
+        onClick={() => (handleClick ? handleClick() : false)}
+      ></div>
+    )
+  );
+}
+
+export default Overlay;
