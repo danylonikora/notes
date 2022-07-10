4676ba999601ca59109e0490721a558602c9c973
Danylo Nikora <d.nikora2019@gmail.com>
2022-07-10 22:34:04 +0300

    Move enableBodyScroll and disableBodyScroll functions in their own file

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Draft from "./components/Draft";
 import Overlay, { OverlayProps } from "./components/Overlay";
 import { useAppSelector } from "./store";
 import { selectNotes } from "./store/notesSlice";
+import { enableBodyScroll, disableBodyScroll } from "./utils/bodyScroll";
 
 export type FiltersT = {
   sort: "ascending" | "descending";
@@ -97,34 +98,6 @@ function App() {
       enableBodyScroll();
     }
   }, [showOverlays]);
-
-  function enableBodyScroll() {
-    if (document.readyState === "complete") {
-      document.body.style.position = "";
-      document.body.style.overflowY = "";
-
-      if (document.body.style.marginTop) {
-        const scrollTop = -parseInt(document.body.style.marginTop, 10);
-        document.body.style.marginTop = "";
-        window.scrollTo(window.pageXOffset, scrollTop);
-      }
-    } else {
-      window.addEventListener("load", enableBodyScroll);
-    }
-  }
-
-  function disableBodyScroll(savePosition = false) {
-    if (document.readyState === "complete") {
-      if (document.body.scrollHeight > window.innerHeight) {
-        if (savePosition)
-          document.body.style.marginTop = `-${window.pageYOffset}px`;
-        document.body.style.position = "fixed";
-        document.body.style.overflowY = "scroll";
-      }
-    } else {
-      window.addEventListener("load", () => disableBodyScroll(savePosition));
-    }
-  }
 
   return (
     <OverlaysContext.Provider
