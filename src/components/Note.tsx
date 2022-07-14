4e5d688c90f369e6470ba7011157ea1b61ea5a8c
Danylo Nikora <d.nikora2019@gmail.com>
2022-07-14 11:34:25 +0300

    Note component: change UI representation of edit and delete buttons, show buttons only after hovering on itself

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -3,9 +3,6 @@ import type { draftState } from "../App";
 import { useAppDispatch } from "../store";
 import { removeNote } from "../store/notesSlice";
 
-import pencilPng from "../assets/pencil.png";
-import trashPng from "../assets/trash.png";
-
 export type NoteT = {
   readonly id: string;
   html: string;
@@ -41,12 +38,30 @@ function Note({ data, setDraft }: NoteProps) {
   return (
     <>
       <div className="note">
-        <time
-          className="note__date"
-          dateTime={new Date(Date.now()).toISOString()}
-        >
-          {formatLastUpdateTime()}
-        </time>
+        <div className="note__date-and-btns-container">
+          <time
+            className="note__date"
+            dateTime={new Date(Date.now()).toISOString()}
+          >
+            {formatLastUpdateTime()}
+          </time>
+          <div className="note__btns">
+            <button
+              className="note__edit-btn note__btn"
+              title="Edit note"
+              onClick={() => {
+                setDraft({ mode: "editing", note: data });
+              }}
+            />
+            <button
+              className="note__delete-btn note__btn"
+              title="Delete note"
+              onClick={() => {
+                dispatch(removeNote(data.id));
+              }}
+            />
+          </div>
+        </div>
         <ul className="note__tags">
           {data.tags.map((tag) => (
             <li className="note__tag" key={tag}>
@@ -59,26 +74,6 @@ function Note({ data, setDraft }: NoteProps) {
           ref={noteContentRef}
           dangerouslySetInnerHTML={{ __html: data.html }}
         />
-        <div className="note__btns">
-          <button
-            className="note__edit-btn note__btn"
-            onClick={() => {
-              setDraft({ mode: "editing", note: data });
-            }}
-          >
-            <span>Edit</span>
-            <img className="note__icon" src={pencilPng} />
-          </button>
-          <button
-            className="note__delete-btn note__btn"
-            onClick={() => {
-              dispatch(removeNote(data.id));
-            }}
-          >
-            <span>Delete</span>
-            <img className="note__icon" src={trashPng} />
-          </button>
-        </div>
       </div>
     </>
   );
